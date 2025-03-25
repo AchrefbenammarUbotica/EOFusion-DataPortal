8379bfa3b3f47276138169779e853e6b2242dd52
achrefbenammar404 <achref.benammar404@gmail.com>
2025-03-25 16:11:25 +0100

    Make ingest_vessel and ingest_satellite functions asynchronous

--- src/routers/ingestion.py
+++ src/routers/ingestion.py
@@ -6,14 +6,14 @@ from src.schemas.data_schema import Vessel, Satellite
 router = APIRouter()
 
 @router.post("/vessels/")
-def ingest_vessel(vessel: Vessel, session: Session = Depends(get_session)):
+async def ingest_vessel(vessel: Vessel, session: Session = Depends(get_session)):
     session.add(vessel)
     session.commit()
     session.refresh(vessel)
     return vessel
 
 @router.post("/satellites/")
-def ingest_satellite(satellite: Satellite, session: Session = Depends(get_session)):
+async def ingest_satellite(satellite: Satellite, session: Session = Depends(get_session)):
     session.add(satellite)
     session.commit()
     session.refresh(satellite)
